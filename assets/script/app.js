b7b6f94d0c705a030dfc900455dd3c4e378f57a3
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-05-16 00:43:42 -0300

    Add ASIDE com SIDEBAR para INFO do DEV

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,10 +12,10 @@ let weekend = new Array(
 )
 let day = dateToday.getDate()
 let month = new Array(
-    'Janeiro', 'Fevereiro', 
-    'Março', 'Abril', 
-    'Maio', 'Junho', 
-    'Julho', 'Agosto', 
+    'Janeiro', 'Fevereiro',
+    'Março', 'Abril',
+    'Maio', 'Junho',
+    'Julho', 'Agosto',
     'Setembro', 'Outubro',
     'Novembro', 'Dezembro'
 )
@@ -27,3 +27,17 @@ let completeDate = String(weekend[dateToday.getDay()] + ", " + day + " de " + mo
 let subtitle = document.getElementById('date')
 
 subtitle.innerHTML = completeDate
+
+
+// Criando a funcionalidade do sidebard
+
+function openNav() {
+    document.getElementById("opened").style.width = "250px";
+    document.getElementById("collapsed").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("opened").style.width = "0";
+    document.getElementById("collapsed").style.marginLeft = "0";
+}
+
